dee6c9e691ba8ec06fca95fecaad954d47041871
abhishekkp00 <abhishekforcollege@gmail.com>
2026-09-16 19:43:11 +0530

    Configured Flyway and Database V1

--- Api-management-service/src/main/java/com/abhishek/gatryx/management/SmartManagementServiceApplication.java
+++ Api-management-service/src/main/java/com/abhishek/gatryx/management/SmartManagementServiceApplication.java
@@ -2,13 +2,11 @@ package com.abhishek.gatryx.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class SmartManagementServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartManagementServiceApplication.class, args);
 	}
-
 }
